b1fb7afb0639dc9f074cefbc1701b3fd0a6f2dc6
Abdulaziz Alharbi <abdulazizalharbi@Abdulazizs-MacBook-Pro.local>
2020-07-24 22:38:25 +0300

    creating a new branch

---
+++ MapLocationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapLocationTest
+//
+//  Created by Abdulaziz Alharbi on 03/12/1441 AH.
+//  Copyright © 1441 Abdulaziz Alharbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
